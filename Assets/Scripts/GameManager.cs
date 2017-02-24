f8c257ff0eb9770f96b41b2ab4475e272edeedf3
Luca <luca.falco00@gmail.com>
2017-02-24 11:33:19 +0100

    ordine cartelle e movimento e camera migliorati

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+
+
+	public GameObject player1;
+	public GameObject player2;
+
+	/*[HideInInspector]*/ public PlayerControl player1Control;
+	/*[HideInInspector]*/ public PlayerControl player2Control;
+
+	//da nascondere in inspector probs
+
+
+	void Awake(){
+		instance = this;
+
+		player1Control = player1.GetComponent<PlayerControl>();
+		player2Control = player2.GetComponent<PlayerControl>();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		player1Control.playerNumber = 1;
+		player2Control.playerNumber = 2;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	private float horizontalMovement;
+	private float verticalMovement;
+	private Rigidbody rb;
+	public float speed;
+	public float rotSpeed;
+
+	public int playerNumber;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		horizontalMovement = Input.GetAxis ("Horizontal" + playerNumber);
+		verticalMovement = Input.GetAxis ("Vertical" + playerNumber);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Move ();
+	}
+
+	public void Move()
+	{
+		//DA APPROFONDIRE MEGLIO
+
+		//movement
+		//rb.AddForce (transform.right * horizontalMovement * speed * Time.deltaTime,ForceMode.Impulse);
+		//rb.AddForce (transform.forward * verticalMovement * speed * Time.deltaTime, ForceMode.Impulse);
+
+		Vector3 movePositionForward = transform.forward * verticalMovement * speed * Time.deltaTime;
+		rb.MovePosition (rb.position + movePositionForward);
+
+		//turning
+		//da vedere Quaternion.LookRotation
+		Quaternion turnRotationHorizontal = Quaternion.Euler (0f, horizontalMovement * rotSpeed * Time.deltaTime, 0f);
+		rb.MoveRotation (rb.rotation * turnRotationHorizontal);
+
+	}
+
+
+}

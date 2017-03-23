1fbd11c2d35244e44b68011916721b4d489089f5
CARLO\user2 <carlo93.cg@gmail.com>
2017-03-23 20:10:19 +0100

    Postman first version

---
+++ Assets/Scripts/PostMan.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostMan : MonoBehaviour
+{
+    public NavMeshAgent postMan;
+    public Transform[] path;
+    public float postManSpeed;
+    private int pathIndex=0;
+    private bool alive;
+
+	// Use this for initialization
+	void Awake ()
+    {
+	  postMan = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        alive = true;
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PostManMove();
+    }
+
+    void PostManMove()
+    {
+            postMan.speed = postManSpeed;
+
+            postMan.destination = path[pathIndex].transform.position;
+
+            if (Vector3.Distance(postMan.transform.position, path[pathIndex].transform.position) <= 0.5)
+            {
+
+                pathIndex++;
+                Debug.Log(pathIndex);
+
+                if (pathIndex == path.Length)
+                {
+                    alive = false;
+                    Destroy(this.gameObject);
+                }
+            }
+    }
+}

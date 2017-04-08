5823d9f5968a406113994e0b627ea4b2aea957d1
Luca <luca.falco00@gmail.com>
2017-04-08 12:47:23 +0200

    Fix Shader su bullet

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,34 +2,41 @@
 
 public class CameraControl : MonoBehaviour
 {
-	public float dampTime = 0.2f;                
-	public float screenEdgeBuffer = 4f;           
+	public float dampTime = 0.2f;
+	public float screenEdgeBuffer = 4f;
 	public float minSize = 6.5f;
-    private float zoomSpeed;
+	private float zoomSpeed;
 
-    private Vector3 moveVelocity;
-    private Vector3 desiredPosition;
+	private Vector3 moveVelocity;
+	private Vector3 desiredPosition;
 
-    public Shader OutlineShader;
+	public Shader OutlineShader;
 	public Shader StandardShader;
 
-	private Camera mainCamera;                        
+	private Camera mainCamera;
 
-    public Transform[] targets;
+	public Transform[] targets;
 
-    private void Awake ()
+	private OutlineShaderApply shaderApply;
+	private MeshRenderer[] playerMeshRenderer;
+
+	void Awake ()
 	{
+		shaderApply = new OutlineShaderApply ();
+
 		GameObject[] players;
 		players = GameManager.instance.GetPlayers ();
 
 		targets = new Transform[players.Length];
+		playerMeshRenderer = new MeshRenderer[players.Length];
 
-		for (int playerIndex=0; playerIndex < players.Length; playerIndex++) {
+		for (int playerIndex = 0; playerIndex < players.Length; playerIndex++) {
 			targets [playerIndex] = players [playerIndex].transform;
+
+			playerMeshRenderer [playerIndex] = players [playerIndex].GetComponent<MeshRenderer> ();
 		}
 		mainCamera = GetComponentInChildren<Camera> ();
 	}
-
 
 	private void FixedUpdate ()
 	{
@@ -43,7 +50,7 @@ public class CameraControl : MonoBehaviour
 	private void Move ()
 	{
 		FindAveragePosition ();
-		transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref moveVelocity, dampTime);
+		transform.position = Vector3.SmoothDamp (transform.position, desiredPosition, ref moveVelocity, dampTime);
 	}
 
 
@@ -53,8 +60,7 @@ public class CameraControl : MonoBehaviour
 		int numTargets = 0;
 
 
-		for (int i = 0; i < targets.Length; i++)
-		{
+		for (int i = 0; i < targets.Length; i++) {
 			
 			if (targets [i].gameObject.activeSelf) {
 				averagePos += targets [i].position;
@@ -66,26 +72,25 @@ public class CameraControl : MonoBehaviour
 		if (numTargets > 0) {
 			averagePos /= numTargets;
 		}
-			averagePos.y = transform.position.y;
-			desiredPosition = averagePos;
+		averagePos.y = transform.position.y;
+		desiredPosition = averagePos;
 	}
 
 
 	private void Zoom ()
 	{
 		
-		float requiredSize = FindRequiredSize();
+		float requiredSize = FindRequiredSize ();
 		mainCamera.orthographicSize = Mathf.SmoothDamp (mainCamera.orthographicSize, requiredSize, ref zoomSpeed, dampTime);
 	}
 
 
 	private float FindRequiredSize ()
 	{
-		Vector3 desiredLocalPos = transform.InverseTransformPoint(desiredPosition);
+		Vector3 desiredLocalPos = transform.InverseTransformPoint (desiredPosition);
 		float size = 0f;
 
-		for (int i = 0; i < targets.Length; i++)
-		{
+		for (int i = 0; i < targets.Length; i++) {
 			if (targets [i].gameObject.activeSelf) {
 				Vector3 targetLocalPos = transform.InverseTransformPoint (targets [i].position);
 				Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
@@ -106,30 +111,23 @@ public class CameraControl : MonoBehaviour
 		mainCamera.orthographicSize = FindRequiredSize ();
 	}
 
-	public	void WallDetection(){
-			int playerNumbers = GameManager.instance.numberOfPlayers;
+	public	void WallDetection ()
+	{
+		int playerNumbers = GameManager.instance.numberOfPlayers;
 			
-			RaycastHit hit;
-			GameObject[] players = new GameObject[playerNumbers];
-			Vector3[] screenPos = new Vector3[playerNumbers];
-			Ray[] wallRay = new Ray[playerNumbers];
+		RaycastHit hit;
+		GameObject[] players = new GameObject[playerNumbers];
+		Vector3[] screenPos = new Vector3[playerNumbers];
+		Ray[] wallRay = new Ray[playerNumbers];
 
-			for(int playerIndex = 0; playerIndex < GameManager.instance.GetPlayers().Length ; playerIndex++ ){
+		for (int playerIndex = 0; playerIndex < GameManager.instance.GetPlayers ().Length; playerIndex++) {
 			
 			players [playerIndex] = GameManager.instance.GetPlayers () [playerIndex];
-			screenPos[playerIndex] = mainCamera.ScreenToWorldPoint (players[playerIndex].transform.position);
-			wallRay[playerIndex] = mainCamera.ScreenPointToRay (screenPos[playerIndex]);
-			Vector3 dir = players[playerIndex].transform.position - mainCamera.transform.position;
+			screenPos [playerIndex] = mainCamera.ScreenToWorldPoint (players [playerIndex].transform.position);
+			wallRay [playerIndex] = mainCamera.ScreenPointToRay (screenPos [playerIndex]);
 
-			if (Physics.Raycast (mainCamera.transform.position, dir ,out hit )) {
-				if (hit.transform.tag == "Wall") {
-//					Debug.Log ("Wall Found!");
-					players [playerIndex].GetComponent<MeshRenderer> ().material.shader = OutlineShader;
-				} else {
-					players [playerIndex].GetComponent<MeshRenderer> ().material.shader = StandardShader;
-				}
-			}
-			Debug.DrawRay (mainCamera.transform.position,dir,Color.green);
+			Vector3 dir = players [playerIndex].transform.position - mainCamera.transform.position;
+			shaderApply.ShaderApply (playerMeshRenderer [playerIndex], players [playerIndex].transform.position, OutlineShader, StandardShader);
 		}
 	}
 }

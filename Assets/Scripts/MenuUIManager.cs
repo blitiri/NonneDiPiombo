f3e856f359dee78d84cac8a2f187f2ab97c1b8b3
blitiri <blitiri@gmail.com>
2017-04-22 21:46:43 +0200

    Correzioni menu scelta personaggio

---
+++ Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Menu user interface manager.
+/// </summary>
+public class MenuUIManager : AbstractUIManager
+{
+	/// <summary>
+	/// The players labels.
+	/// </summary>
+	public UILabel[] playersLabels;
+
+	void Start ()
+	{
+		InitUI ();
+	}
+
+	/// <summary>
+	/// Inits the UI.
+	/// </summary>
+	public void InitUI ()
+	{
+		int playerIndex;
+
+		for (playerIndex = 0; playerIndex < playersLabels.Length; playerIndex++) {
+			playersLabels [playerIndex].color = playersColors [playerIndex];
+		}
+	}
+}

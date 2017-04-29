7a68593d3f8b16cf4dcd09f7b1350a7a93ea759b
Marco <blitiri@gmail.com>
2017-04-29 15:22:07 +0200

    Interfaccia di un generico oggetto danneggiabile (giocatore o altro oggetto)

---
+++ Assets/Scripts/IDamageableObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Interface of a damageable object.
+/// </summary>
+public interface IDamageableObject
+{
+	/// <summary>
+	/// Adds the damage.
+	/// </summary>
+	/// <param name="demage">Demage to add.</param>
+	void AddDamage (int demage);
+}

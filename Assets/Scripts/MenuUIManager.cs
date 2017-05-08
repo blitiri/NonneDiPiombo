ddc14a6b63dcd87e2ad751d8b4082dc41841b753
blitiri <blitiri@gmail.com>
2017-05-08 07:53:34 +0200

    Completata implementazione menu

--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -11,6 +11,31 @@ public class MenuUIManager : AbstractUIManager
 	/// </summary>
 	[Tooltip ("The name of the next scene to load with start button.")]
 	public string nextSceneName = "CharacterSelection";
+	/// <summary>
+	/// The volume slider.
+	/// </summary>
+	public UISlider volumeSlider;
+	/// <summary>
+	/// The checked sprite.
+	/// </summary>
+	public UISprite checkedSprite;
+	/// <summary>
+	/// The options window.
+	/// </summary>
+	public UIPanel optionsWindow;
+	/// <summary>
+	/// The credits window.
+	/// </summary>
+	public UIPanel creditsWindow;
+
+	/// <summary>
+	/// Start the script.
+	/// </summary>
+	void Start ()
+	{
+		optionsWindow.gameObject.SetActive (false);
+		creditsWindow.gameObject.SetActive (false);
+	}
 
 	/// <summary>
 	/// Starts the game.
@@ -20,12 +45,24 @@ public class MenuUIManager : AbstractUIManager
 		SceneController.instance.LoadSceneByName (nextSceneName);
 	}
 
+	/// <summary>
+	/// Opens options window.
+	/// </summary>
 	public void OnOptions ()
 	{
+		Debug.Log ("Open - Fullscreen: " + Configuration.instance.IsFullScreen ());
+		Debug.Log ("Open - Sound volume: " + Configuration.instance.GetSoundVolume ());
+		checkedSprite.enabled = Configuration.instance.IsFullScreen ();
+		volumeSlider.value = Configuration.instance.GetSoundVolume ();
+		optionsWindow.gameObject.SetActive (true);
 	}
 
+	/// <summary>
+	/// Opens credits windows.
+	/// </summary>
 	public void OnCredits ()
 	{
+		creditsWindow.gameObject.SetActive (true);
 	}
 
 	/// <summary>
@@ -34,5 +71,28 @@ public class MenuUIManager : AbstractUIManager
 	public void OnQuit ()
 	{
 		Application.Quit ();
+	}
+
+	/// <summary>
+	/// Set fullscreen flag.
+	/// </summary>
+	public void OnFullscreen ()
+	{
+		checkedSprite.enabled = !checkedSprite.enabled;
+	}
+
+	/// <summary>
+	/// Closes a window.
+	/// </summary>
+	/// <param name="window">Window to close.</param>
+	public void OnClose (GameObject window)
+	{
+		window.SetActive (false);
+		if (window.name.Equals ("OptionsWindow")) {
+			Configuration.instance.SetFullScreen (checkedSprite.enabled);
+			Configuration.instance.SetSoundVolume (volumeSlider.value); 
+			Debug.Log ("Close - Fullscreen: " + Configuration.instance.IsFullScreen ());
+			Debug.Log ("Close - Sound volume: " + Configuration.instance.GetSoundVolume ());
+		}
 	}
 }

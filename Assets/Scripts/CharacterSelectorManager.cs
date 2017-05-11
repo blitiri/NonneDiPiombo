d14107680baa531dfc5827fafeffff7848bbbf26
BatManBatManBatMan <imbatmanyear1@gmail.com>
2017-05-11 17:34:29 +0200

    Working on character Selection

--- Assets/Scripts/CharacterSelectorManager.cs
+++ Assets/Scripts/CharacterSelectorManager.cs
@@ -2,64 +2,122 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class CharacterSelectorManager : MonoBehaviour {
-
-    private const int maxNumberOfPlayers = 4;
-    [Range(2, maxNumberOfPlayers)]
-    public int numberOfPlayers = 2;
-    public int numberOfDifferentGrannies = 2;
-
-    public static CharacterSelectorManager instance;
-
-    public Sprite[] grannieIcons;
-
-    public GameObject[] playerSelectors;
+public class CharacterSelectorManager : MonoBehaviour
+{
+    /// <summary>
+    /// the maximum number of possible players.
+    /// </summary>
+    public const int maxNumberOfPlayers = 4;
+    /// <summary>
+    /// The true number of players.
+    /// </summary>
+    [Range(2,4)]
+    public int numberOfPlayers = 3;
+    /// <summary>
+    /// An instance of SceneController class.
+    /// </summary>
+    private SceneController mySceneContr;
+    /// <summary>
+    /// The indexes needed to switch between grannies. One for each player.
+    /// </summary>
+    public int[] indexes;
+    /// <summary>
+    /// All the grannies.
+    /// </summary>
+    public string[] iconNames;
+    /// <summary>
+    /// All the selectors.
+    /// </summary>
+    public GameObject[] selectors;
+    /// <summary>
+    /// All playable grannies.
+    /// </summary>
     public GameObject[] grannies;
-    public GameObject[] playerSelectedGrannies;
+    /// <summary>
+    /// Grannies selected by each player.
+    /// </summary>
+    public GameObject[] selectedGrannies;
+    /// <summary>
+    /// An icon for each selector.
+    /// </summary>
+    public UIButton[] centrals;
 
-    void Awake()
+    private void Awake()
     {
-        instance = this;
-        DontDestroyOnLoad(transform.gameObject);
-        playerSelectedGrannies = new GameObject[numberOfPlayers];
+        DontDestroyOnLoad(gameObject);
+        indexes = new int[numberOfPlayers];
+        selectedGrannies = new GameObject[numberOfPlayers];
+        mySceneContr = new SceneController();
         SelectorsActivation();
-        //    grannyIcons = new Sprite[numberOfDifferentGrannys];
-        //    playerSelectors = new GameObject[maxNumberOfPlayers];
-        //    grannys = new GameObject[numberOfDifferentGrannys];
     }
 
-    void Start ()
+    private void Start()
     {
-        for (int i = 0; i < playerSelectedGrannies.Length; i++)
-        {
-            playerSelectedGrannies[i] = grannies[0];
-        }
-    }
-	
-	void Update ()
-    {
-
+        GrannieAssignation();
     }
 
-    void SelectorsActivation()
+    private void SelectorsActivation()
     {
         for (int i = 0; i < numberOfPlayers; i++)
         {
-            playerSelectors[i].SetActive(true);
-            CharacterSelectorController thisPlayerSelectorController = playerSelectors[i].GetComponent<CharacterSelectorController>();
-            if (thisPlayerSelectorController.myPlayer == null)
-            {
-                thisPlayerSelectorController.myPlayer = i;
-            }
+            selectors[i].SetActive(true);
         }
         for (int i = numberOfPlayers; i < maxNumberOfPlayers; i++)
         {
-            playerSelectors[i].SetActive(false);
-            CharacterSelectorController thisPlayerSelectorController = playerSelectors[i].GetComponent<CharacterSelectorController>();
-            if (thisPlayerSelectorController.myPlayer == null)
-            {
-                thisPlayerSelectorController.myPlayer = i;
-            }
+            selectors[i].SetActive(false);
         }
+    }
+
+    private void GrannieAssignation()
+    {
+        if (GameManager.instance)
+        {
+            GameManager.instance.players = selectedGrannies;
+            Destroy(gameObject);
+        }
+    }
+
+    public void ClickArrow (GameObject button)
+    {
+        string tag = button.tag;
+        int id = int.Parse(button.transform.parent.tag);
+        //Debug.Log(id);
+        switch (tag)
+        {
+            case "Left":
+                if (indexes[id] > 0)
+                {
+                    indexes[id]--;
+                }
+                else
+                {
+                    indexes[id] = grannies.Length - 1;
+                }
+                break;
+            case "Right":
+                if (indexes[id] < grannies.Length - 1)
+                {
+                    indexes[id]++;
+                }
+                else
+                {
+                    indexes[id] = 0;
+                }
+                break;
+        }
+        Debug.Log(indexes[id]);
+        centrals[id].normalSprite = iconNames[indexes[id]] + "PlayerIcon";
+    }
+
+    public void ClickCentral(GameObject button)
+    {
+        int id = int.Parse(button.transform.parent.tag);
+
+        selectedGrannies[id] = grannies[indexes[id]];
+    }
+
+    public void LoadLevelScene(GameObject button)
+    {
+        mySceneContr.LoadScene(button);
     }
 }
---
+++ Assets/Scripts/IComparerClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IComparerClass : IComparer<GameObject> {
+
+    public int Compare(GameObject x, GameObject y)
+    {
+        if (x.layer < y.layer)
+        {
+            return -1;
+        }
+        else if (x.layer > y.layer)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}

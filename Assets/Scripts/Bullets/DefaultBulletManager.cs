45cd901f12e30890096c9068ffc76b382ff97618
Luca <luca.falco00@gmail.com>
2017-05-20 12:18:24 +0200

    Bullet Structure

---
+++ Assets/Scripts/Bullets/DefaultBulletManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultBulletManager : MonoBehaviour {
+	
+	private MeshRenderer bulletMeshRender;
+	private Rigidbody bulletRb;
+	private CapsuleCollider bulletCollider;
+	private bool isMoving = true;
+	public float bulletSpeed;
+
+	void Awake(){
+		Components ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag.StartsWith ("Player") || other.gameObject.tag.Equals ("Wall")) {
+			Trigger ();
+		}
+	}
+
+	void FixedUpdate(){
+		DefaultMovement (bulletSpeed);
+	}
+
+
+	protected virtual void DefaultMovement(float bulletSpeed){
+		if (isMoving) {
+			bulletRb.velocity = bulletRb.transform.up * bulletSpeed;
+		}
+	}
+
+	protected virtual void Components(){
+		bulletRb = GetComponent<Rigidbody> ();
+		bulletMeshRender = GetComponent<MeshRenderer> ();
+		bulletCollider = GetComponent<CapsuleCollider> ();
+	}
+
+	protected virtual void Trigger(){
+		isMoving = false;
+		bulletMeshRender.enabled = false;
+		bulletCollider.enabled = false;
+		bulletRb.velocity = bulletRb.transform.up * 0;
+		Destroy (this.gameObject, 1f);
+	}
+}

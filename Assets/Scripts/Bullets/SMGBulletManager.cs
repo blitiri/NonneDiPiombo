5b02c1de6293a89e47107361aa61e7236026eb99
Luca <luca.falco00@gmail.com>
2017-05-27 09:45:03 +0200

    Bazooka

--- Assets/Scripts/Bullets/SMGBulletManager.cs
+++ Assets/Scripts/Bullets/SMGBulletManager.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SMGBulletManager : MonoBehaviour {
+public class SMGBulletManager : DefaultBulletManager {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }

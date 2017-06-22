4d3bee0a471834e4b794997037d369eac4b8f4ac
GitHubber <andreafolino.bm@gmail.com>
2017-06-22 17:28:41 +0200

    Configuration

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -127,7 +127,7 @@ public class Configuration : MonoBehaviour
 	/// <param name="numberOfPlayers">Number of players.</param>
 	public void SetNumberOfPlayers (int numberOfPlayers)
 	{
-		this.numberOfPlayers = Mathf.Clamp (minNumberOfPlayers, 0, maxNumberOfPlayers);
+		this.numberOfPlayers = Mathf.Clamp (numberOfPlayers, minNumberOfPlayers, maxNumberOfPlayers);
 		ResizeSelectedGrannies ();
 	}
 

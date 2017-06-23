7e2b5c00588c0433515dccb1534b3c284507fe5a
CARLO\user2 <carlo93.cg@gmail.com>
2017-06-23 21:28:34 +0200

    Inseriti oggetti rotti

---
+++ Assets/Scripts/BrokenObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenObject : MonoBehaviour {
+
+    public float lifeTime = 2.0f;
+    public Rigidbody[] rb;
+
+
+    void Start()
+    {
+        rb = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigid in rb)
+        {
+            rigid.AddForce(rigid.transform.forward * Random.Range(20,40), ForceMode.Impulse);
+            Destroy(this.gameObject, lifeTime);
+        }
+    }
+}

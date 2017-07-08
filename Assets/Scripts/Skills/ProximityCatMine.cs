628408988d62790c7e45afdf5b05bd74ea2eb12d
CARLO\user2 <carlo93.cg@gmail.com>
2017-07-08 12:16:03 +0200

    Bug skill Gattara fixed

--- Assets/Scripts/Skills/ProximityCatMine.cs
+++ Assets/Scripts/Skills/ProximityCatMine.cs
@@ -10,17 +10,17 @@ public class ProximityCatMine : Abilities {
 
 	public override void OnAbilityActivation ()
 	{
-		for (int i = 0; i < players.Length; i++) 
-		{
-			if (players [i] != null) {
-				if(GameManager.instance.catCounter < maxCatNumber)
-				{
-					GameObject cat = Instantiate(catPrefab, players[i].transform.position, Quaternion.identity) as GameObject;
-					cat.tag = "Bullet" + players[i].tag;
-				}
-				players [i] = null;
-			}
-		}
-        
+        if (GameManager.instance.catCounter <= maxCatNumber-1)
+        {
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i] != null)
+                {
+                    GameObject cat = Instantiate(catPrefab, players[i].transform.position, Quaternion.identity) as GameObject;
+                    cat.tag = "Bullet" + players[i].tag;
+                    players[i] = null;
+                }
+            }
+        }
 	}
 }
